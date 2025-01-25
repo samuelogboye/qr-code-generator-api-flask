bed589b9002484be254a251c03934ada1722a497
Samuel Ogboye <ogboyesam@gmail.com>
2025-01-25 23:44:47 +0100

    fix: update config file

--- config.py
+++ config.py
@@ -4,6 +4,13 @@ from datetime import timedelta
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY')
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+    if not SQLALCHEMY_DATABASE_URI:
+        # Construct the URL if DATABASE_URL is not set
+        SQLALCHEMY_DATABASE_URI = (
+            f"postgresql://{os.getenv('DB_USERNAME')}:{os.getenv('DB_PASSWORD')}"
+            f"@{os.getenv('DB_HOST')}:{os.getenv('DB_PORT')}/{os.getenv('DB_DATABASE')}"
+        )
+    print(f"Database URL: {SQLALCHEMY_DATABASE_URI}")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     JWT_SECRET_KEY = os.environ.get('JWT_SECRET_KEY')
     JWT_ACCESS_TOKEN_EXPIRES = timedelta(hours=1)
